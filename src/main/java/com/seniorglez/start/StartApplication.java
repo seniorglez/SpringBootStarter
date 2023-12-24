768c463c4d85152d3a346e62fb799bec37257604
Diego Dominguez <d.dominguez.glez1111@gmail.com>
2023-12-24 22:28:52 +0100

    Injectable annotation

--- src/main/java/com/seniorglez/start/StartApplication.java
+++ src/main/java/com/seniorglez/start/StartApplication.java
@@ -1,9 +1,19 @@
 package com.seniorglez.start;
 
+import com.seniorglez.start.application.Injectable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(
+		basePackages = "com.seniorglez.start",
+		includeFilters = @ComponentScan.Filter(
+				type = FilterType.ANNOTATION,
+				classes = Injectable.class
+		)
+)
 public class StartApplication {
 
 	public static void main(String[] args) {
